1af1571daa53af8e59304792ba7525d2fff69980
Alessandro Febbrai <febbrai.alessandro@libero.it>
2024-12-22 23:55:39 +0100

    ejericio 15

---
+++ ejercicio_15.py
@@ -0,0 +1,30 @@
+""" 
+Definir una clase Empleado que contien dos atributos: el nombre del empleado y su edad.
+
+La clase Empleado debe contener el constructor y los siguientes métodos:
+
+    - __init__(self,...): un constructor que permite inicializar todos los atributos de la clase Empleado.
+      Además, este constructor debe mostrar una cadena de caracteres. (Ver visualización en consola)
+    - __del__(self): un destructor que permite eliminar todas las referencias de un objeto. Este método
+      especial tambièn debe mostrar una cadena de caracteres en la consola.
+
+"""
+
+class Empleado:
+    
+    def __init__(self, n_empleado, edad_empleado):
+        self.n_empleado = n_empleado 
+        self.edad_empleado = edad_empleado
+        print(f'Se ha creado un objeto empleado con nombre {self.n_empleado} y edad {self.edad_empleado} años')
+
+    def __del__(self):
+        print(f'Se ha eliminado el objeto empleado con nombre {self.n_empleado} y edad {self.edad_empleado} años')
+
+empleado_1 = Empleado("Alessandro", 32)
+empleado_2 = Empleado("Giulia", 17)
+
+# Verifica las direcciones de memoria
+print(f"ID de empleado_1: {id(empleado_1)}")
+print(f"ID de empleado_2: {id(empleado_2)}")
+
+del empleado_1
